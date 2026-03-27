fb6577d055a951171a5be7091f85758b4c88e3a9
Osakana <fishnchips.me@gmail.com>
2026-03-27 18:41:52 +0900

    Initial Commit

---
+++ Umbra/UmbraApp.swift
@@ -0,0 +1,15 @@
+
+import SwiftUI
+import CoreData
+
+@main
+struct UmbraApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ UmbraTests/UmbraTests.swift
@@ -0,0 +1,13 @@
+
+import Testing
+@testable import Umbra
+
+struct UmbraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
